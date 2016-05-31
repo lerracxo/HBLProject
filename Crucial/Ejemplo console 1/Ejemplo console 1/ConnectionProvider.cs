e5022d16df7814ef86242eef1dc2384526164f26
lerracxo <lerracxo@gmail.com>
2016-05-31 00:37:22 -0500

    Data

---
+++ Crucial/Ejemplo console 1/Ejemplo console 1/ConnectionProvider.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+
+namespace Ejemplo_console_1
+{
+    class ConnectionProvider
+    {
+        private String DBName;
+        private String DBPort;
+
+
+        public DbConnection getConnection() {
+
+            DataTable table = DbProviderFactories.GetFactoryClasses();
+
+            
+            foreach (DataRow row in table.Rows) {
+                foreach(DataColumn column in table.Columns) {
+                    Console.WriteLine(row[column]);
+                }
+            }
+
+            ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings["MySQL Data Provider"];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(connectionStringSettings.ProviderName);
+
+            DbConnection conn = factory.CreateConnection();
+
+            conn.ConnectionString = connectionStringSettings.ConnectionString;
+            Console.WriteLine(conn);
+            conn.Open();
+            /*
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection();
+
+            try {
+                conn.ConnectionString = "Provider=MySQLProv; Data Source=10.0.2.15,3306; Initial Catalog=test; User ID=root; Password=omaromar";
+                conn.Open();
+            }
+            catch (System.Data.OleDb.OleDbException ex) {
+                Console.WriteLine("Failed to open conecction to Mysql. Details: " + ex.Message);
+            }
+            */
+
+            /*
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            if(settings != null){
+                foreach(ConnectionStringSettings cs in settings)
+                {
+                    Console.WriteLine(cs.ConnectionString);
+                }
+
+            }
+            
+            */
+             /*
+
+            String provider = "MySql.Data.MySqlClient";
+            String connectionString = "SERVER=10.0.2.15;UID=root;PASSWORD=omaromar";
+
+
+
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+            DbConnection conn = factory.CreateConnection();
+
+            conn.ConnectionString = connectionString;
+
+            try
+            {
+                Console.WriteLine("Conexion exitosa");
+            }
+            catch {
+                Console.WriteLine(" La conexión falló");
+
+            }
+            */
+            
+            return conn;
+        }
+
+        public void setDBName(String DBName) {
+            this.DBName = DBName;
+        }
+
+        public void setDBPort(String DBPort) {
+            this.DBPort = DBPort;
+        }
+
+        public String getDBName() {
+            return this.DBName;
+        }
+
+        public String getDBPort() {
+            return this.DBPort;
+        }
+    }
+}
---
+++ Crucial/Ejemplo console 1/Ejemplo console 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_console_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String hola = "hola que onda ";
+            Console.WriteLine(hola);
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < i; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+            ConnectionProvider connection = new ConnectionProvider();
+
+            connection.setDBName("Oracle");
+            connection.setDBPort("969");
+
+            Console.WriteLine(connection.getDBName());
+            Console.WriteLine(connection.getDBPort());
+            Console.WriteLine(connection.getConnection());
+        }
+    }
+}
